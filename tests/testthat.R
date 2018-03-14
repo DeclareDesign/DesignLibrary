56ba12714aa5bbc4c91de149358a76604d6ea6e2
Jasper Cooper <jaspercooper@gmail.com>
2018-03-14 14:28:00 -0400

    tests in

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(designs)
+
+test_check("designs")
---
+++ tests/testthat/test-designs.R
@@ -0,0 +1,51 @@
+context("Test Designs")
+
+test_that("Mediation works", {
+  
+  vary_N_mediation <- fill_out(template = mediation_template, rho = seq(-1,1,.2))
+  
+  diagnosis <- DeclareDesign::diagnose_design(vary_N_mediation,sims = 10,bootstrap = F)
+  
+  diagnosis
+
+  })
+
+
+test_that("Two-Way Factorial works", {
+  
+  two_way <- two_way_factorial_template()
+  
+  DeclareDesign::diagnose_design(two_way, sims = 10,bootstrap = F)
+  
+  vary_N_2way <- fill_out(template = two_way_factorial_template, N = c(100,200,300))
+  
+  DeclareDesign::diagnose_design(vary_N_2way, sims = 10,bootstrap = F)
+  
+})
+
+test_that("Regression Discontinuity works", {
+  
+  
+  RD <- regression_discontinuity_template()
+  
+  DeclareDesign::diagnose_design(RD, sims = 100,bootstrap = F)
+  
+  vary_N_RD <-
+    fill_out(template = regression_discontinuity_template, N = c(100, 200, 300))
+  
+  DeclareDesign::diagnose_design(vary_N_RD, sims = 100, bootstrap = F)
+  
+})
+
+test_that("Two Arm works", {
+  
+  two_arm <- two_arm_template()
+  
+  DeclareDesign::diagnose_design(two_arm, sims = 100,bootstrap = F)
+  
+  vary_N_two_arm <-
+    fill_out(template = two_arm_template, N = c(200, 300, 400))
+  
+  DeclareDesign::diagnose_design(vary_N_two_arm, sims = 100, bootstrap = F)
+  
+})
