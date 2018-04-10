477a3b1878eb8ffccccc7338756650a1d9452e5b
Neal Fultz <nfultz@gmail.com>
2018-04-10 18:03:18 +0000

    rename

--- tests/testthat/test-designs.R
+++ tests/testthat/test-designs.R
@@ -2,7 +2,7 @@ context("Test Designs")
 
 test_that("Mediation works", {
 
-  vary_N_mediation <- fill_out(template = mediation_template, rho = seq(-1,1,.2))
+  vary_N_mediation <- expand_design(template = mediation_template, rho = seq(-1,1,.2))
 
   diagnosis <- DeclareDesign::diagnose_design(vary_N_mediation,sims = 10,bootstrap = F)
 
@@ -17,7 +17,7 @@ test_that("Two-Way Factorial works", {
 
   DeclareDesign::diagnose_design(two_way, sims = 10,bootstrap = F)
 
-  vary_N_2way <- fill_out(template = two_way_factorial_template, N = c(100,200,300))
+  vary_N_2way <- expand_design(template = two_way_factorial_template, N = c(100,200,300))
 
   DeclareDesign::diagnose_design(vary_N_2way, sims = 10,bootstrap = F)
 
@@ -31,7 +31,7 @@ test_that("Regression Discontinuity works", {
   DeclareDesign::diagnose_design(RD, sims = 100,bootstrap = F)
 
   vary_N_RD <-
-    fill_out(template = regression_discontinuity_template, N = c(100, 200, 300))
+    expand_design(template = regression_discontinuity_template, N = c(100, 200, 300))
 
   DeclareDesign::diagnose_design(vary_N_RD, sims = 100, bootstrap = F)
 
@@ -44,7 +44,7 @@ test_that("Two Arm works", {
   DeclareDesign::diagnose_design(two_arm, sims = 100,bootstrap = F)
 
   vary_N_two_arm <-
-    fill_out(template = two_arm_template, N = c(200, 300, 400))
+    expand_design(template = two_arm_template, N = c(200, 300, 400))
 
   DeclareDesign::diagnose_design(vary_N_two_arm, sims = 100, bootstrap = F)
 
@@ -57,7 +57,7 @@ test_that("Randomized Response works", {
   DeclareDesign::diagnose_design(randomized_response, sims = 100,bootstrap = F)
 
   vary_N_randomized_response <-
-    fill_out(template = randomized_response_template, N = c(200, 300, 400))
+    expand_design(template = randomized_response_template, N = c(200, 300, 400))
 
   DeclareDesign::diagnose_design(vary_N_randomized_response, sims = 100, bootstrap = F)
 
@@ -68,7 +68,7 @@ test_that("Noncompliance works", {
   noncompliance <- noncompliance_template()
   DeclareDesign::diagnose_design(noncompliance, sims = 10, bootstrap = F)
 
-  vary_N_noncompliance <- fill_out(template = noncompliance_template, N = c(200, 300, 400))
+  vary_N_noncompliance <- expand_design(template = noncompliance_template, N = c(200, 300, 400))
   DeclareDesign::diagnose_design(vary_N_noncompliance, sims = 10, bootstrap = F)
 
 })
@@ -78,7 +78,7 @@ test_that("List Experiment works", {
   list_experiment <- list_experiment_template()
   diagnose_design(list_experiment, sims = 100, bootstrap = F)
 
-  vary_N_list_experiment <- fill_out(template = list_experiment_template, N = c(200, 300, 400))
+  vary_N_list_experiment <- expand_design(template = list_experiment_template, N = c(200, 300, 400))
   DeclareDesign::diagnose_design(vary_N_list_experiment, sims = 10, bootstrap = F)
 
 })
@@ -95,7 +95,7 @@ test_that("Matching works", {
   DeclareDesign::diagnose_design(match_out, sims = 100, bootstrap = F,
                   diagnosands = matching_diagnosands)
 
-  vary_N_matching <- fill_out(template = matching_template, N = c(200, 300, 400))
+  vary_N_matching <- expand_design(template = matching_template, N = c(200, 300, 400))
   DeclareDesign::diagnose_design(vary_N_matching, sims = 10, bootstrap = F, diagnosands = matching_diagnosands)
 
 
@@ -116,7 +116,7 @@ test_that("Process Tracing works",{
   DeclareDesign::diagnose_design(process_tracing, sims = 100,bootstrap = F,diagnosands = process_tracing_diagnosands)
 
   vary_N_process_tracing <-
-    fill_out(template = process_tracing_template, N = c(200, 300, 400))
+    expand_design(template = process_tracing_template, N = c(200, 300, 400))
 
   DeclareDesign::diagnose_design(vary_N_process_tracing, sims = 100,
                                  bootstrap = F,
@@ -128,7 +128,7 @@ test_that("Election Forecast works", {
   election_forecast <- election_forecast_template()
   DeclareDesign::diagnose_design(election_forecast, sims = 10, bootstrap = F)
 
-  vary_N_election_forecast <- fill_out(template = election_forecast_template, N = c(200, 300, 400))
+  vary_N_election_forecast <- expand_design(template = election_forecast_template, N = c(200, 300, 400))
   DeclareDesign::diagnose_design(vary_N_election_forecast, sims = 10, bootstrap = F)
 
 })
@@ -138,7 +138,7 @@ test_that("SRS works", {
   srs <- srs_template()
   DeclareDesign::diagnose_design(srs, sims = 10, bootstrap = F)
 
-  vary_N_srs <- fill_out(template = srs_template, n = c(200, 300, 400))
+  vary_N_srs <- expand_design(template = srs_template, n = c(200, 300, 400))
   DeclareDesign::diagnose_design(vary_N_srs, sims = 10, bootstrap = F)
 
 })
@@ -149,7 +149,7 @@ test_that("Audit works", {
   DeclareDesign::diagnose_design(audit, sims = 10, bootstrap = F)
 
   vary_N_audit <-
-    fill_out(template = audit_template, N = c(200, 300, 400))
+    expand_design(template = audit_template, N = c(200, 300, 400))
   DeclareDesign::diagnose_design(vary_N_audit, sims = 10, bootstrap = F)
 
 })
@@ -163,7 +163,7 @@ test_that("Crossover works", {
   DeclareDesign::diagnose_design(crossover, sims = 100, bootstrap = F)
 
   vary_rho_crossover <-
-    fill_out(template = crossover_template, rho = seq(.1,.9,.1))
+    expand_design(template = crossover_template, rho = seq(.1,.9,.1))
   DeclareDesign::diagnose_design(vary_rho_crossover, sims = 10, bootstrap = F)
 
 })
@@ -175,7 +175,7 @@ test_that("Blocked and Clustered works", {
   DeclareDesign::diagnose_design(blocked_and_clustered, sims = 100, bootstrap = F)
 
   vary_icc_blocked_and_clustered <-
-    fill_out(template = blocked_and_clustered_template, cluster_icc = seq(.1,.5,.1))
+    expand_design(template = blocked_and_clustered_template, cluster_icc = seq(.1,.5,.1))
   DeclareDesign::diagnose_design(vary_icc_blocked_and_clustered, sims = 10, bootstrap = F)
 
 })
