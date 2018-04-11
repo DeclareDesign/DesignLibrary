e94e292101f1b6f348a706c46f2f3763b9dfda1d
macartan <macartan@gmail.com>
2018-04-11 07:34:40 -0400

    Simple two arm template
    Has possible correlations between Y1 and Y0.  Designer has attributes
    and can export code.

---
+++ R/simple_two_arm_designer.R
@@ -0,0 +1,52 @@
+#' Create a simple two arm design
+#' @param N Number of units
+#' @param code If TRUE template  returns the code of a design 
+#' @return a function that returns a design
+#' @export
+#'
+#' @examples
+#' simple_two_arm_design <- simple_two_arm_designer()
+#' simple_two_arm_designer(code = TRUE)
+
+
+simple_two_arm_designer <- function(N = 100,
+                                    prob = .5,
+                                    control_mean = 0,
+                                    control_sd = 1,
+                                    ate = 1,
+                                    treatment_mean = control_mean + ate,
+                                    treatment_sd = control_sd,
+                                    rho = 1,
+                                    code = FALSE
+){
+  # Below is grabbed by get_design_code
+  
+design_code <- function()  {{{
+    # M: Model
+    pop <- declare_population(
+      N = 100,
+      Z0 = rnorm(N, mean = control_mean, sd = control_sd),
+      Z1 = correlate(given = Z0, rho = rho, rnorm, mean = treatment_mean, sd = treatment_sd)
+    )
+    potential_outcomes <- declare_potential_outcomes(Y ~ Z0 + Z * Z1)
+    
+    # I: Inquiry
+    estimand <- declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0))
+    
+    # D: Data
+    assignment <- declare_assignment(prob = prob)
+    
+    # A: Analysis
+    estimator <- declare_estimator(Y ~ Z, estimand = estimand)
+    
+    # Design
+    pop / potential_outcomes / estimand /assignment / declare_reveal() /estimator
+}}}    
+
+if(code)  out <- get_design_code(design_code)
+if(!code) out <- design_code()
+return(out)
+}
+
+attr(simple_two_arm_designer, "shiny_args") <- list(N = c(10, 20, 50), ate = c(0, .5)) 
+
