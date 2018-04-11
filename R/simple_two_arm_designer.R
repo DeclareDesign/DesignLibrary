369eb309634590fc5821df267364a71328162d31
Jasper Cooper <jaspercooper@gmail.com>
2018-04-11 16:42:50 +0200

    Simple two-arm designer now returns the simple_two_arm design outside of triple braces [ci skip]

--- R/simple_two_arm_designer.R
+++ R/simple_two_arm_designer.R
@@ -21,31 +21,33 @@ simple_two_arm_designer <- function(N = 100,
 ){
   # Below is grabbed by get_design_code
   
-design_code <- function()  {{{
-    # M: Model
-    pop <- declare_population(
-      N = 100,
-      Z0 = rnorm(N, mean = control_mean, sd = control_sd),
-      Z1 = correlate(given = Z0, rho = rho, rnorm, mean = treatment_mean, sd = treatment_sd)
-    )
-    potential_outcomes <- declare_potential_outcomes(Y ~ Z0 + Z * Z1)
-    
-    # I: Inquiry
-    estimand <- declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0))
-    
-    # D: Data
-    assignment <- declare_assignment(prob = prob)
-    
-    # A: Analysis
-    estimator <- declare_estimator(Y ~ Z, estimand = estimand)
-    
-    # Design
-    pop / potential_outcomes / estimand /assignment / declare_reveal() /estimator
-}}}    
-
-if(code)  out <- get_design_code(design_code)
-if(!code) out <- design_code()
-return(out)
+  design_code <- function(){  
+    {{{
+      # M: Model
+      pop <- declare_population(
+        N = 100,
+        Z0 = rnorm(N, mean = control_mean, sd = control_sd),
+        Z1 = correlate(given = Z0, rho = rho, rnorm, mean = treatment_mean, sd = treatment_sd)
+      )
+      potential_outcomes <- declare_potential_outcomes(Y ~ Z0 + Z * Z1)
+      
+      # I: Inquiry
+      estimand <- declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0))
+      
+      # D: Data
+      assignment <- declare_assignment(prob = prob)
+      
+      # A: Analysis
+      estimator <- declare_estimator(Y ~ Z, estimand = estimand)
+      
+      # Design
+      simple_two_arm <- pop / potential_outcomes / estimand /assignment / declare_reveal() /estimator
+    }}}    
+    simple_two_arm
+  }
+  if(code)  out <- get_design_code(design_code)
+  if(!code) out <- design_code()
+  return(out)
 }
 
 attr(simple_two_arm_designer, "shiny_args") <- list(N = c(10, 20, 50), ate = c(0, .5)) 
