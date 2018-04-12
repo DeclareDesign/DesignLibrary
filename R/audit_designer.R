a79bd2dfa52565d8443d6920ac01d05a66bc4759
Jasper Cooper <jaspercooper@gmail.com>
2018-04-12 17:08:35 +0200

    Audit experiment designer is in

---
+++ R/audit_designer.R
@@ -0,0 +1,99 @@
+#' Create an audit experiment design
+#'
+#' Description here
+#' 
+#' Key limitations: Limitations here.
+#' 
+#' Note: Note here.
+#'
+#' @param code Logical. If TRUE, returns the code of a design, otherwise returns a design.
+#' @param N Size of sample
+#' @return An audit experiment design.
+#' @export
+#'
+#' @examples
+#' # To make a design using default arguments:
+#' audit_experiment_design <- audit_experiment_designer()
+#'
+#' # To export DeclareDesign code for a design:
+#' audit_experiment_designer(code = TRUE)
+
+
+audit_experiment_designer <- function(N = 100,code = FALSE) 
+  {
+    design_code <- function() {
+      # Below is grabbed by get_design_code
+      {{{
+        # M: Model
+        population <-
+          declare_population(
+            N = N,
+            discrimination = rnorm(N),
+            type = draw_ordered(
+              discrimination,
+              breaks = c(-Inf, 0, 0.5, 2, Inf),
+              break_labels = c("A", "B", "C", "D")
+            )
+          )
+        
+        potential_outcomes <-
+          declare_potential_outcomes(
+            R_Z_0 = as.numeric(type %in% c("A", "C")),
+            R_Z_1 = as.numeric(type %in% c("A", "B")),
+            Y_Z_0 = ifelse(
+              type %in% c("A", "C"),
+              draw_binary(latent = discrimination, link = "probit"),
+              NA
+            ),
+            Y_Z_1 = ifelse(
+              type %in% c("A", "B"),
+              draw_binary(latent = discrimination + 0.25, link = "probit"),
+              NA
+            ),
+            Ystar_Z_0 = ifelse(type %in% c("A", "C"), Y_Z_0, 0),
+            Ystar_Z_1 = ifelse(type %in% c("A", "B"), Y_Z_1, 0)
+          )
+        
+        # I: Inquiry
+        estimand_1 <-
+          declare_estimand(mean(R_Z_1 - R_Z_0), label = "ATE on Response")
+        estimand_2 <-
+          declare_estimand(mean(Y_Z_1[type == "A"] - Y_Z_0[type == "A"]), label = "ATE on Tone")
+        estimand_3 <-
+          declare_estimand(mean(Ystar_Z_1 - Ystar_Z_0), label = "ATE on Tone (Alternative)")
+        
+        # D: Data Strategy
+        assignment <- declare_assignment(prob = 0.5)
+        
+        # A: Answer Strategy
+        estimator_1 <-
+          declare_estimator(R ~ Z, estimand = estimand_1, label = "ATE on Response")
+        estimator_2 <-
+          declare_estimator(Y ~ Z, estimand = estimand_2, label = "ATE on Tone")
+        estimator_3 <-
+          declare_estimator(Ystar ~ Z, estimand = estimand_3, label = "ATE on Tone (Alternative)")
+        
+        # Design
+        audit_experiment_design <- declare_design(
+          population,
+          potential_outcomes,
+          assignment,
+          estimand_1,
+          estimand_2,
+          estimand_3,
+          declare_reveal(outcome_variables = c("R", "Y", "Ystar")),
+          estimator_1,
+          estimator_2,
+          estimator_3
+        )
+      }}}
+      audit_experiment_design
+    }
+    if (code)
+      out <- get_design_code(design_code)
+    else
+      out <- design_code()
+    return(out)
+  }
+attr(audit_experiment_designer, "tips") <- c(N = "Size of sample")
+attr(audit_experiment_designer, "shiny_arguments") <- c(N = c(100, 500, 1000))
