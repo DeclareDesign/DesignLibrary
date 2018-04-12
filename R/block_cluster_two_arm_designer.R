f77222c4d85644f02a50b197f4ec238c4b7493c3
macartan <macartan@gmail.com>
2018-04-12 07:32:15 -0400

    Adding cluster size var to data

--- R/block_cluster_two_arm_designer.R
+++ R/block_cluster_two_arm_designer.R
@@ -1,11 +1,13 @@
 #' Create a simple two arm design
-#' @param N Number of units
-#' @param code If TRUE template  returns the code of a design 
+#' 
+#' Note: Default arguments produce a design without blocks and clusters and with N determined by `N_cluster_in_block`
+#' 
+#' @param code If TRUE template returns the code of a design 
 #' @param N_blocks Number of blocks
-#' @param sd_block Standard deviation of block level shocks
-#' @param N_cluster_in_block Number of clusters per block
-#' @param sd_cluster Standard deviation of cluster level shock
+#' @param N_clusters_in_block Number of clusters per block
 #' @param N_i_in_cluster Individuals per block
+#' @param sd_block Standard deviation of block level shocks
+#' @param sd_cluster Standard deviation of cluster level shock
 #' @param sd_i Standard deviation of individual level shock 
 #' @param prob Assignment probability
 #' @param control_mean Average outcome in control
@@ -19,11 +21,12 @@
 #' block_cluster_two_arm_designer(code = TRUE)
 
 
-block_cluster_two_arm_designer <- function(N_blocks = 2,
+block_cluster_two_arm_designer <- function(
+                                    N_blocks = 1,
+                                    N_clusters_in_block = 100,
+                                    N_i_in_cluster = 1,
                                     sd_block = .2,
-                                    N_cluster_in_block = 2,
                                     sd_cluster = .2,
-                                    N_i_in_cluster = 2,
                                     sd_i = max(0, 1 - sd_block - sd_cluster),
                                     prob = .5,
                                     control_mean = 0,
@@ -31,19 +34,25 @@ block_cluster_two_arm_designer <- function(N_blocks = 2,
                                     treatment_mean = control_mean + ate,
                                     code = FALSE
 ){
-  # Below is grabbed by get_design_code
   
+  # Below is grabbed by get_design_code
+
+    
 design_code <- function() { 
+  
   {{{
   
   # M: Model
    pop <- declare_population(
-      blocks   = add_level(N = N_blocks, u_b = rnorm(N)*sd_block),
-      clusters = add_level(N = N_cluster_in_block, u_c = rnorm(N)*sd_cluster),
-      i  = add_level( N  = N_i_in_cluster, 
-                      u_i = rnorm(N)*sd_i,
-                      Z0 = u_i+ u_b + u_c ,
-                      Z1 = Z0 +   treatment_mean)
+      blocks   = add_level(N = N_blocks, 
+                           u_b = rnorm(N)*sd_block),
+      clusters = add_level(N = N_clusters_in_block, 
+                           u_c = rnorm(N)*sd_cluster,
+                           cluster_size = N_i_in_cluster),
+      i        = add_level(N  = N_i_in_cluster, 
+                           u_i = rnorm(N)*sd_i,
+                           Z0 = u_i + u_b + u_c,
+                           Z1 = Z0 + treatment_mean)
       )
     
    potential_outcomes <- declare_potential_outcomes(Y ~ (1-Z)*Z0 + Z * Z1)
