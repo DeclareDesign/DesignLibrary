588ec52bec1f0148a4c2d41aa0e5d9d82facfc17
Jasper Cooper <jaspercooper@gmail.com>
2018-04-12 11:29:24 +0200

    Tested the new contribute_design function using 'a_new_simple_design'

---
+++ R/a_new_simple_design.R
@@ -0,0 +1,11 @@
+#' A new simple design
+#' 
+#' This is just a test
+#' 
+#' @docType data 
+#' @keywords datasets 
+#' @name a_new_simple_design
+#' @usage summary(a_new_simple_design)
+#' @format A design
+
+'a_new_simple_design'
