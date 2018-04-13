5efbb584c6f6a61aca07b2ec188c7b33be038b39
Lily Medina <>
2018-04-13 16:36:30 +0200

    corrections

--- R/make_vignette.R
+++ R/make_vignette.R
@@ -78,82 +78,100 @@ diagnosis <- diagnose_design(',file_name,'_design, sims = ',sims,', bootstrap = 
 
 make_text <- function(text, text_path){
   
-  if(!is.null(text_path)){ 
-    rmarkdown::render(text_path, quiet = TRUE, output_file = "DD_temp_rmd.Rmd")
-    file.remove("DD_temp_rmd.Rmd")
-    p <- readLines(text_path)
-    text <- paste0(p[grep("```", p)[1]:length(p)], sep = "\n")
+  if(!is.null(text_path))
+  { 
+    text <- readLines(text_path)
+    if(endsWith(text_path, ".Rmd"))
+    {
+     rmarkdown::render(text_path, quiet = TRUE, output_file = "DD_temp_rmd.Rmd")
+     file.remove("DD_temp_rmd.Rmd")
+     text <- paste0(p[grep("```", text)[1]:length(text)], sep = "\n")
+     }
   }
   text
 }
 
 #' Create a simple two arm design
 #' @param design_or_designer A design or a designer
-#' @param vignette_title A character string 
-#' @param front_text A character string 
-#' @param end_text A character string 
-#' @param front_text_path Path to .Rmd; If different from NULL, it overwrites front_text
-#' @param end_text_path   Path to .Rmd; If different from NULL, it overwrites end_text
-#' @param sims number of simulations for DeclareDesign
-#' @param bootstraps  number of bootstraps for DeclareDesign 
+#' @param title A character string for the vignette title.  If NULL then a default based on the name of design_or_designer.
+#' @param front_text A character string.
+#' @param end_text A character string.
+#' @param front_text_path Path to .Rmd; If different from NULL, it overwrites front_text.
+#' @param end_text_path   Path to .Rmd; If different from NULL, it overwrites end_text.
+#' @param sims Number of simulations for DeclareDesign.
+#' @param bootstraps  Number of bootstraps for DeclareDesign 
 #' @return This function creates a .Rmd 
 #' @export 
 #'
 
-make_vignette <- function(design_or_designer = NULL, title = NULL, front_text = "", end_text = "", front_text_path = NULL, end_text_path = NULL , sims = 1000, bootstrap = FALSE){
+make_vignette <- function(design_or_designer = NULL,
+                          title = NULL,
+                          front_text = "",
+                          end_text = "",
+                          front_text_path = NULL,
+                          end_text_path = NULL ,
+                          sims = 1000,
+                          bootstrap = FALSE) {
+  
   options(error = NULL)
   object_name <- deparse(substitute(design_or_designer))
+  if (is.null(title))
+    title <-  gsub("_", replacement = " ", object_name)
   shiny = ""
   class_a <- class(design_or_designer)
   has_shiny <- FALSE
   
-  if(any(class_a  == "design") ) {
-  if(!endsWith(object_name, "_design"))
-    stop("Design's name must end with suffix '_design'")
+  if (any(class_a  == "design")) {
+    if (!endsWith(object_name, "_design"))
+      stop("Design's name must end with suffix '_design'")
     make_chunks <- make_design_chunks
+    object_name <- sub("_design", replacement = "", object_name)
   }
-  else if(is.function(design_or_designer)) 
+  else if (is.function(design_or_designer))
   {
-    class_a <- class(design_or_designer()) 
-    if(!any(class_a  == "design" ))
-      stop("Argument 'design_or_designer' must either be an object of class 'design' or a designer that returns an object of class 'design'")
-    else if(!endsWith(object_name, "_designer")) 
+    class_a <- class(design_or_designer())
+    if (!any(class_a  == "design"))
+      stop(
+        "Argument 'design_or_designer' must either be an object of class 'design' 
+         or a designer that returns an object of class 'design'"
+      )
+    else if (!endsWith(object_name, "_designer"))
       stop("Designer's name must end with suffix '_designer'")
-    else 
+    else
     {
       has_shiny <- !is.null(get_shiny_arguments(design_or_designer))
-      object_name <- sub("_designer", replacement = "", object_name) 
+      object_name <- sub("_designer", replacement = "", object_name)
       make_chunks <- make_designer_chunks
     }
   }
-  else  
-    stop("Argument 'design_or_designer' must either be an object of class 'design' or a designer that returns an object of class 'design'")
+  else
+    stop(
+      "Argument 'design_or_designer' must either be an object of class 'design'
+      or a designer that returns an object of class 'design'"
+    )
   
-  #title_ <- object_name
   file_name <- tolower(object_name)
-
-  if(is.null(title)) title <-  gsub("_", replacement = " ", object_name) 
   
-  if(has_shiny) shiny <- 
-    paste0('
+  if (has_shiny)
+    shiny <- paste0(' 
 <!--
-  <a class="btn btn-primary" href="http://shiny.declaredesign.org/inspector/?topic=',file_name,'">
-     <i class="fa fa-area-chart" title = "Go to the design inspector" fa-2x></i> Inspect design
-   </a>
--->'
-    
-  )
- 
+   <a class="btn btn-primary" href="http://shiny.declaredesign.org/inspector/?topic=',file_name,'">
+    <i class="fa fa-area-chart" title = "Go to the design inspector" fa-2x></i> Inspect design
+  </a>
+-->')
+  
   front_text <- make_text(front_text, front_text_path)
   end_text   <- make_text(end_text, end_text_path)
   
-  if(file.exists(paste0(file_name, ".Rmd")))
+  if (file.exists(paste0(file_name, ".Rmd")))
     stop("Vignette already exists")
+  
   file.create(paste0(file_name, ".Rmd"))
-  cat(make_header(title, shiny, file_name ),
+  cat(make_header(title, shiny, file_name),
       front_text,
       make_chunks(file_name, sims, bootstrap, has_shiny),
-      end_text, sep ="\n", file = paste0(file_name, ".Rmd"))
-  
+      end_text,
+      sep = "\n",
+      file = paste0(file_name, ".Rmd"))
 }
 
