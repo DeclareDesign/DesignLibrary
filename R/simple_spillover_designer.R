5f5fc5fc0e169cac7e6bd6201c567bf5bda672ae
macartan <macartan@gmail.com>
2018-04-17 11:19:18 +0200

    change spillovers to equal sharing model

--- R/simple_spillover_designer.R
+++ R/simple_spillover_designer.R
@@ -1,13 +1,17 @@
 #' Create a simlpe design with spillovers
 #'
 #' This designer builds a design with \code{n_groups} groups each containing \code{group_size} individuals. 
-#' An individual is affected if anyone in her group is treated. The default estimand is the average difference 
+#' Potential outcomes exhibit spillovers: if any individual in a group receives treatment, 
+#' the effect is equally spread among members of the group. 
+#' Intuitively, for ever $1 given to a member of a group, each member receives $1\code{groupsize}
+#' The default estimand is the average difference 
 #' across subjects between no one treated and only that subject treated.  
 #' 
-#' #'
+#' 
 #' @param code Logical. If TRUE, returns the code of a design, otherwise returns a design.
 #' @param N_groups Number of groups.
 #' @param group_size Number of units in each group
+#' @param sd Standard deviation of individual level shock
 #' @return A function that returns a design.
 #' @export
 #'
@@ -28,7 +32,7 @@ simple_spillover_designer <- function(n_groups = 40, group_size = 3, sd =1, code
       # Model ----------------------------------------------------------------------
       pop <- declare_population(N = n_groups*group_size, 
                                 G = rep(1:n_groups, each = group_size))
-      dgp <- function(i, Z, G) (sum(Z[G == G[i]])>0) + rnorm(1)*sd
+      dgp <- function(i, Z, G) sum(Z[G == G[i]])/n_groups + rnorm(1)*sd
       
       # Inquiry --------------------------------------------------------------------
       estimand <- declare_estimand(ATE = mean(
