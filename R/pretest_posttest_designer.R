af4329edfd9553d226e3f0468ff65be144e763d1
Jasper Cooper <jaspercooper@gmail.com>
2018-05-07 15:56:51 -0400

    Updated pretest_posttest_designer to new code-grabbing method

--- R/pretest_posttest_designer.R
+++ R/pretest_posttest_designer.R
@@ -6,7 +6,6 @@
 #'
 #' Note: Note here.
 #'
-#' @param code Logical. If TRUE, returns the code of a design, otherwise returns a design.
 #' @param N An integer. Size of sample.
 #' @param ate A number. Average treatment effect
 #' @param rho A number in [0,1]. Correlation in outcomes between pre- and post-test
@@ -18,75 +17,68 @@
 #' # To make a design using default arguments:
 #' pretest_posttest_design <- pretest_posttest_designer()
 #'
-#' # To export DeclareDesign code for a design:
-#' pretest_posttest_designer(code = TRUE)
 pretest_posttest_designer <- function(N = 100,
                                       ate = .25,
                                       rho = .5,
-                                      attrition_rate = .1,
-                                      code = FALSE)
+                                      attrition_rate = .1)
 {
-  design_code <- function() {
-    # Below is grabbed by get_design_code
-    {{{
-      # M: Model
-      pop <- declare_population(
-        N = N,
-        u_t1 = rnorm(N, 0, sqrt(1 - rho)),
-        u_t2 = rnorm(N, 0, sqrt(1 - rho)),
-        u_i = rnorm(N, 0, sqrt(rho))
-      )
-      pos_t1 <- declare_potential_outcomes(Y_t1 ~ u_i + u_t1)
-      pos_t2 <- declare_potential_outcomes(Y_t2 ~ ate * Z + u_i + u_t2)
-      report <- declare_assignment(m = round(N * (1 - attrition_rate)),
-                                   assignment_variable = R)
-      # I: Inquiry
-      estimand <- declare_estimand(ATE = mean(Y_t2_Z_1 - Y_t2_Z_0))
-      # D: Data Strategy
-      assignment <-
-        declare_assignment(m = round(N / 2), assignment_variable = Z)
-      # A: Answer Strategy
-      pretest_lhs <- declare_estimator((Y_t2 - Y_t1) ~ Z,
-                                       model = lm_robust,
-                                       estimand = estimand,
-                                       subset = R == 1,
-                                       label = "Change score"
-      )
-      pretest_rhs <- declare_estimator(
-        Y_t2 ~ Z + Y_t1,
-        model = lm_robust,
-        estimand = estimand,
-        subset = R == 1,
-        label = "Condition on pretest"
-      )
-      posttest_only <- declare_estimator(
-        Y_t2 ~ Z,
-        model = lm_robust,
-        estimand = estimand,
-        label = "Posttest only"
-      )
-      # Design
-      pretest_posttest_design <- declare_design(
-        pop,
-        pos_t1,
-        pos_t2,
-        estimand,
-        assignment,
-        report,
-        declare_reveal(Y_t1),
-        declare_reveal(Y_t2),
-        pretest_lhs,
-        pretest_rhs,
-        posttest_only
-      )
-    }}}
-    pretest_posttest_design
-  }
-  if (code)
-    out <- get_design_code(design_code)
-  else
-    out <- design_code()
-  return(out)
+  {{{
+    # M: Model
+    pop <- declare_population(
+      N = N,
+      u_t1 = rnorm(N, 0, sqrt(1 - rho)),
+      u_t2 = rnorm(N, 0, sqrt(1 - rho)),
+      u_i = rnorm(N, 0, sqrt(rho))
+    )
+    pos_t1 <- declare_potential_outcomes(Y_t1 ~ u_i + u_t1)
+    pos_t2 <- declare_potential_outcomes(Y_t2 ~ ate * Z + u_i + u_t2)
+    report <- declare_assignment(m = round(N * (1 - attrition_rate)),
+                                 assignment_variable = R)
+    # I: Inquiry
+    estimand <- declare_estimand(ATE = mean(Y_t2_Z_1 - Y_t2_Z_0))
+    # D: Data Strategy
+    assignment <-
+      declare_assignment(m = round(N / 2), assignment_variable = Z)
+    # A: Answer Strategy
+    pretest_lhs <- declare_estimator((Y_t2 - Y_t1) ~ Z,
+                                     model = lm_robust,
+                                     estimand = estimand,
+                                     subset = R == 1,
+                                     label = "Change score"
+    )
+    pretest_rhs <- declare_estimator(
+      Y_t2 ~ Z + Y_t1,
+      model = lm_robust,
+      estimand = estimand,
+      subset = R == 1,
+      label = "Condition on pretest"
+    )
+    posttest_only <- declare_estimator(
+      Y_t2 ~ Z,
+      model = lm_robust,
+      estimand = estimand,
+      label = "Posttest only"
+    )
+    # Design
+    pretest_posttest_design <- declare_design(
+      pop,
+      pos_t1,
+      pos_t2,
+      estimand,
+      assignment,
+      report,
+      declare_reveal(Y_t1),
+      declare_reveal(Y_t2),
+      pretest_lhs,
+      pretest_rhs,
+      posttest_only
+    )
+  }}}
+  
+  attr(pretest_posttest_design, "code") <- 
+    construct_design_code(pretest_posttest_designer, match.call.defaults())
+  
+  pretest_posttest_design
 }
 attr(pretest_posttest_designer, "shiny_arguments") <- list(
   N = c(100, 50, 1000),
@@ -100,3 +92,22 @@ attr(pretest_posttest_designer, "tips") <- c(
   rho = "Correlation in outcomes between pre- and post-test",
   attrition_rate = "Proportion of respondents lost when using pre-test data"
 )
+attr(pretest_posttest_designer, "description") <- "
+<p> A pretest-posttest design with sample of size <code>N</code>, average treatment effect of size <code>ate</code>, 
+    and correlation between pre- and post-test outcomes equal to  <code>rho</code>. The proportion of pre-test respondents  
+   missing at random from  the post-test follow-up can be set using  <code>attrition_rate</code>.
+"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
