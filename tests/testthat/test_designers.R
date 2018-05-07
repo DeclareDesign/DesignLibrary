238800287712889c82659098c5e5f3c1b6b72176
Jasper Cooper <jaspercooper@gmail.com>
2018-05-07 16:42:08 -0400

    Fixed tests to work with new code-getting method
    - I also tried running them and all designers passed

--- tests/testthat/test_designers.R
+++ tests/testthat/test_designers.R
@@ -9,65 +9,52 @@ for(designer in designers){
   the_designer <- get(x = designer)
   has_shiny <- !is.null(attributes(the_designer)$shiny_arguments)
   designer_args <- formals(the_designer)
+  designer_attr <- attributes(the_designer)
+  one_design <- the_designer()
+  design_attr <- attributes(one_design)
   
   testthat::test_that(
-    desc = paste0(designer," should have a logical code argument"),
+    desc = paste0("designs returned by ", designer," should have a code attribute"),
     code = {
-      expect_true("code" %in% names(designer_args)) 
+      expect_true("code" %in% names(design_attr)) 
     }
   )
   
   testthat::test_that(
     desc = paste0(designer," returns a DD-type design."),
     code = {
-      expect_true("design" %in% class(the_designer()))
+      expect_true("design" %in% class(one_design))
     })
   
   testthat::test_that(
     desc = paste0(designer,"'s default design runs."),
     code = {
-      test_design <- the_designer()
-      diagnose_design(test_design,sims = 5,bootstrap = F)
+      diagnose_design(one_design,sims = 5,bootstrap = F)
     })
   
   testthat::test_that(
-    desc = paste0(designer, " should return code as a character string when code = TRUE"),
+    desc = paste0(designer, " should return designs that have code as a character string in attributes"),
     code = {
-      expect_true(class(the_designer(code = TRUE)) == "character")
+      expect_true(class(design_attr$code) == "character")
     })
 
   testthat::test_that(
     desc = paste0(designer, " uses declare_design declaration somewhere"),
     code = {
-      expect_true(any(grepl("declare_design|/",the_designer(code = TRUE))))
+      expect_true(any(grepl("declare_design|/",design_attr$code)))
     })
   
   testthat::test_that(
     desc = paste0("Code inside ",designer, " runs and creates an appropriately named design object."),
     code = {
-      eval(parse(text = designer_default_args_text(the_designer)))
-      eval(parse(text = the_designer(code = TRUE)))
+      eval(parse(text = design_attr$code))
       expect_true(exists(x = gsub("_designer\\b","_design",designer)))
     })
   
-  testthat::test_that(
-    desc = paste0(designer," is able to make vignettes."),
-    code = {
-      make_vignette(design_or_designer = the_designer,title = "Test Vignette",overwrite = TRUE,front_text = "Blah",end_text = "Blah")
-    })
-  
-  # testthat::test_that(
-  #   desc = paste0("contribute_design works with ",designer,"."),
-  #   code = {
-  #     contribute_design(the_designer,designer,description = "test design")
-  #     file.remove("R/the_designer.R")
-  #     file.remove("data/the_designer.Rdata")
-  #   }
-  # )
   
   if(has_shiny){
     
-    shiny_arguments <- attributes(the_designer)$shiny_arguments
+    shiny_arguments <- designer_attr$shiny_arguments
     
     testthat::test_that(
       desc = paste0("Any shiny_arguments in the attributes of ",designer," should all be in the its formals."),
@@ -90,7 +77,7 @@ for(designer in designers){
     testthat::test_that(
       desc = paste0("get_shiny_diagnosis works with ",designer," for at least 5 sims."),
       code = {
-        get_shiny_diagnosis(designer = the_designer,sims = 5)
+        get_shiny_diagnosis(designer = the_designer,sims = 2)
       }
     )
   }
