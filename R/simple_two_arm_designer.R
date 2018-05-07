d58f8f3be35f35d1f92430de628001323761f723
Jasper Cooper <jaspercooper@gmail.com>
2018-05-07 16:18:42 -0400

    Updated simple_two_arm_designer to new code-grabbing method

--- R/simple_two_arm_designer.R
+++ R/simple_two_arm_designer.R
@@ -6,7 +6,6 @@
 #'
 #' @examples
 #' simple_two_arm_design <- simple_two_arm_designer()
-#' simple_two_arm_designer(code = TRUE)
 
 
 simple_two_arm_designer <- function(N = 100,
@@ -16,38 +15,35 @@ simple_two_arm_designer <- function(N = 100,
                                     ate = 1,
                                     treatment_mean = control_mean + ate,
                                     treatment_sd = control_sd,
-                                    rho = 1,
-                                    code = FALSE
+                                    rho = 1
 ){
-  # Below is grabbed by get_design_code
   
-  design_code <- function(){  
-    {{{
-      # M: Model
-      pop <- declare_population(
-        N = N,
-        Z0 = rnorm(N, mean = control_mean, sd = control_sd),
-        Z1 = correlate(given = Z0, rho = rho, rnorm, mean = treatment_mean, sd = treatment_sd)
-      )
-      potential_outcomes <- declare_potential_outcomes(Y ~ (1-Z) * Z0 + Z * Z1)
-      
-      # I: Inquiry
-      estimand <- declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0))
-      
-      # D: Data
-      assignment <- declare_assignment(prob = prob)
-      
-      # A: Analysis
-      estimator <- declare_estimator(Y ~ Z, estimand = estimand)
-      
-      # Design
-      simple_two_arm_design <- pop / potential_outcomes / estimand /assignment / declare_reveal() /estimator
-    }}}    
-    simple_two_arm_design
-  }
-  if(code)  out <- get_design_code(design_code)
-  if(!code) out <- design_code()
-  return(out)
+  {{{
+    # M: Model
+    pop <- declare_population(
+      N = N,
+      Z0 = rnorm(N, mean = control_mean, sd = control_sd),
+      Z1 = correlate(given = Z0, rho = rho, rnorm, mean = treatment_mean, sd = treatment_sd)
+    )
+    potential_outcomes <- declare_potential_outcomes(Y ~ (1-Z) * Z0 + Z * Z1)
+    
+    # I: Inquiry
+    estimand <- declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0))
+    
+    # D: Data
+    assignment <- declare_assignment(prob = prob)
+    
+    # A: Analysis
+    estimator <- declare_estimator(Y ~ Z, estimand = estimand)
+    
+    # Design
+    simple_two_arm_design <- pop / potential_outcomes / estimand / assignment / declare_reveal() / estimator
+  }}}
+  
+  attr(simple_two_arm_design, "code") <- 
+    construct_design_code(simple_two_arm_designer, match.call.defaults())
+  
+  simple_two_arm_design
 }
 
 attr(simple_two_arm_designer, "shiny_arguments") <- list(N = c(10, 20, 50), ate = c(0, .5)) 
