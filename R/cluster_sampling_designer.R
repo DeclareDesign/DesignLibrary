1679dc126dfeb4d591d9c1ec1de47c166d92abd7
Jasper Cooper <jaspercooper@gmail.com>
2018-05-07 15:23:08 -0400

    updated cluster_sampling_designer to new code-grabbing method

--- R/cluster_sampling_designer.R
+++ R/cluster_sampling_designer.R
@@ -6,7 +6,6 @@
 #' 
 #' Note: Note here.
 #'
-#' @param code Logical. If TRUE, returns the code of a design, otherwise returns a design.
 #' @param N_clusters An integer. Total number of clusters in the population.
 #' @param N_subjects_per_cluster An integer. Total number of subjects per cluster in the population.
 #' @param n_clusters An integer. Number of clusters to sample.
@@ -19,61 +18,55 @@
 #' # To make a design using default arguments:
 #' cluster_sampling_design <- cluster_sampling_designer()
 #'
-#' # To export DeclareDesign code for a design:
-#' cluster_sampling_designer(code = TRUE)
 #'
 
 cluster_sampling_designer <- function(n_clusters = 100,
                                       n_subjects_per_cluster = 10,
                                       icc = 0.2,
                                       N_clusters = 1000,
-                                      N_subjects_per_cluster = 50,
-                                      code = FALSE)
-{
+                                      N_subjects_per_cluster = 50
+){
   if(n_clusters > N_clusters) stop(paste0("n_clusters must be smaller than the total number of ", N_clusters, " subjects."))
   if(n_subjects_per_cluster > N_subjects_per_cluster) stop(paste0("n_subjects_per_cluster must be smaller than the maximum of ", N_subjects_per_cluster, " subjects per cluster."))
-  design_code <- function() {
-    {{{
-      # M: Model
-      population <-
-        declare_population(
-          cluster = add_level(N = N_clusters),
-          subject = add_level(N = N_subjects_per_cluster,
-                              latent_ideology = draw_normal_icc(mean = 0, N = N, clusters = cluster, ICC = icc),
-                              Y = draw_ordered(x = latent_ideology, breaks = qnorm(seq(0, 1, length.out = 8)))
-          )
+  {{{
+    # M: Model
+    population <-
+      declare_population(
+        cluster = add_level(N = N_clusters),
+        subject = add_level(N = N_subjects_per_cluster,
+                            latent_ideology = draw_normal_icc(mean = 0, N = N, clusters = cluster, ICC = icc),
+                            Y = draw_ordered(x = latent_ideology, breaks = qnorm(seq(0, 1, length.out = 8)))
         )
-      fixed_pop <- population()
-      
-      # I: Inquiry
-      estimand <- declare_estimand(mean(Y), label = "Ybar")
-      
-      # D: Data Strategy
-      stage_1_sampling <- declare_sampling(clusters = cluster, n = n_clusters)
-      stage_2_sampling <- declare_sampling(strata = cluster, n = n_subjects_per_cluster)
-      
-      # A: Answer Strategy
-      no_clustering <- declare_estimator(Y ~ 1,
-                                         model = lm_robust,
-                                         estimand = estimand, 
-                                         label = "Standard Errors Not Clustered")
-      clustered_ses <- declare_estimator(Y ~ 1,
-                                         model = lm_robust,
-                                         clusters = cluster,
-                                         estimand = estimand,
-                                         label = "Clustered Standard Errors")
-      
-      
-      # Design
-      cluster_sampling_design <- declare_design(fixed_pop, estimand, stage_1_sampling, stage_2_sampling, no_clustering, clustered_ses)
-    }}}
-    cluster_sampling_design 
-  }
-  if (code)
-    out <- get_design_code(design_code)
-  else
-    out <- design_code()
-  return(out)
+      )
+    fixed_pop <- population()
+    
+    # I: Inquiry
+    estimand <- declare_estimand(mean(Y), label = "Ybar")
+    
+    # D: Data Strategy
+    stage_1_sampling <- declare_sampling(clusters = cluster, n = n_clusters)
+    stage_2_sampling <- declare_sampling(strata = cluster, n = n_subjects_per_cluster)
+    
+    # A: Answer Strategy
+    no_clustering <- declare_estimator(Y ~ 1,
+                                       model = lm_robust,
+                                       estimand = estimand, 
+                                       label = "Standard Errors Not Clustered")
+    clustered_ses <- declare_estimator(Y ~ 1,
+                                       model = lm_robust,
+                                       clusters = cluster,
+                                       estimand = estimand,
+                                       label = "Clustered Standard Errors")
+    
+    
+    # Design
+    cluster_sampling_design <- declare_design(fixed_pop, estimand, stage_1_sampling, stage_2_sampling, no_clustering, clustered_ses)
+  }}}
+  
+  attr(cluster_sampling_design, "code") <- 
+    construct_design_code(cluster_sampling_designer, match.call.defaults())
+  
+  cluster_sampling_design 
 }
 attr(cluster_sampling_designer, "tips") <- list(
   n_clusters = "Number of clusters to sample",
@@ -85,7 +78,11 @@ attr(cluster_sampling_designer, "shiny_arguments") <- list(
   n_subjects_per_cluster = seq(10, 40, 10),
   icc = c(0.2, seq(0.002, .999, by = 0.2))
 )
-
+attr(cluster_sampling_designer, "description") <- "
+<p> A cluster sampling design with <code>n_clusters</code> clusters each comprising 
+    <code>n_subjects_per_cluster</code> units. Outcomes within clusters have ICC approximately equal to 
+    <code>ICC</code>. 
+"
 
 
 
