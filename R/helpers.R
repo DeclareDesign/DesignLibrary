bbd2e048bbe8f520867b74f57922bce4d2e8c8d0
Clara Bicalho <cbm343@nyu.edu>
2018-05-24 11:37:38 +0200

    Use deparse() when srcref not available

--- R/helpers.R
+++ R/helpers.R
@@ -10,10 +10,21 @@ get_design_code <- function(design){
 #' @export
 construct_design_code <- function(designer, args){
   # get the code for the design 
-  txt <- capture.output(designer)
-  
-  open <- grep("[{]{3}", txt)
-  close <- grep("[}]{3}", txt)
+  txt <- as.character(getSrcref(designer))
+  if(length(txt)==0){
+    txt <- capture.output(designer)
+    x <- grep("[{]", txt)
+    open <- x[which(diff(x) == 1)]
+    if(length(open)>3) stop("More than three consecutive `{` found in ", substitute(designer))
+    open <- open[3]+1
+    x <- grep("[}]", txt)
+    close <- x[which(diff(x) == 1)]
+    if(length(close)>2) stop("More than three consecutive `}` found in ", substitute(designer))
+    close <- close[1]
+  }else{
+    open <- grep("[{]{3}", txt)
+    close <- grep("[}]{3}", txt)
+  }
   
   if(length(open) != 1) stop("could not find opening tag in ", substitute(designer))
   if(length(close) != 1) stop("could not find opening tag in ", substitute(designer))
