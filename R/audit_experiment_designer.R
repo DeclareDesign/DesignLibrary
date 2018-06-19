42631ea608b488ab246bc94f415da9e100c685a2
Clara Bicalho <cbm343@nyu.edu>
2018-06-19 16:48:05 +0200

    Adapt terms to post-treatment case

--- R/audit_experiment_designer.R
+++ R/audit_experiment_designer.R
@@ -23,7 +23,7 @@ audit_experiment_designer <- function(N = 100
     population <-
       declare_population(
         N = N,
-        discrimination = rnorm(N),
+        latent_trait = rnorm(N),
         type = draw_ordered(
           discrimination,
           breaks = c(-Inf, 0, 0.5, 2, Inf),
@@ -37,12 +37,12 @@ audit_experiment_designer <- function(N = 100
         R_Z_1 = as.numeric(type %in% c("A", "B")),
         Y_Z_0 = ifelse(
           type %in% c("A", "C"),
-          draw_binary(latent = discrimination, link = "probit"),
+          draw_binary(latent = latent_trait, link = "probit"),
           NA
         ),
         Y_Z_1 = ifelse(
           type %in% c("A", "B"),
-          draw_binary(latent = discrimination + 0.25, link = "probit"),
+          draw_binary(latent = latent_trait + 0.25, link = "probit"),
           NA
         ),
         Ystar_Z_0 = ifelse(type %in% c("A", "C"), Y_Z_0, 0),
@@ -51,22 +51,24 @@ audit_experiment_designer <- function(N = 100
     
     # I: Inquiry
     estimand_1 <-
-      declare_estimand(mean(R_Z_1 - R_Z_0), label = "ATE on Response")
+      declare_estimand(mean(R_Z_1 - R_Z_0), label = "ATE on Y1 (Unconditioned)")
     estimand_2 <-
-      declare_estimand(mean(Y_Z_1[type == "A"] - Y_Z_0[type == "A"]), label = "ATE on Tone")
+      declare_estimand(mean(Y_Z_1 - Y_Z_0), label = "ATE on Y2 (Conditioned by Y1)")
     estimand_3 <-
-      declare_estimand(mean(Ystar_Z_1 - Ystar_Z_0), label = "ATE on Tone (Alternative)")
+      declare_estimand(mean(Y_Z_1[type == "A"] - Y_Z_0[type == "A"]), label = "ATE on Y2 (When Y2 Always Observed)")
+    estimand_4 <-
+      declare_estimand(mean(Ystar_Z_1 - Ystar_Z_0), label = "ATE on Y2 (Alternative)")
     
     # D: Data Strategy
     assignment <- declare_assignment(prob = 0.5)
     
     # A: Answer Strategy
     estimator_1 <-
-      declare_estimator(R ~ Z, estimand = estimand_1, label = "ATE on Response")
+      declare_estimator(R ~ Z, estimand = estimand_1, label = "ATE on Y1 (Unconditioned)")
     estimator_2 <-
-      declare_estimator(Y ~ Z, estimand = estimand_2, label = "ATE on Tone")
+      declare_estimator(Y ~ Z, estimand = c(estimand_2, estimand_3), label = "ATE on Y2 (Conditioned by Y1)")
     estimator_3 <-
-      declare_estimator(Ystar ~ Z, estimand = estimand_3, label = "ATE on Tone (Alternative)")
+      declare_estimator(Ystar ~ Z, estimand = estimand_3, label = "ATE on Y2 (Alternative)")
     
     # Design
     audit_experiment_design <- population +
@@ -75,6 +77,7 @@ audit_experiment_designer <- function(N = 100
       estimand_1 +
       estimand_2 +
       estimand_3 +
+      estimand_4 +
       declare_reveal(outcome_variables = c("R", "Y", "Ystar")) +
       estimator_1 +
       estimator_2 +
