b051fd3067359e5c360dfe797a5777eaca186de2
Jasper Cooper <jaspercooper@gmail.com>
2018-07-25 16:20:34 -0400

    More merge issues

--- R/simple_spillover_designer.R
+++ R/simple_spillover_designer.R
@@ -1,62 +1,64 @@
 #' Create a simple design with spillovers
 #'
-#' This designer builds a design with \code{n_groups} groups each containing \code{group_size} individuals. 
+#' Builds a design with \code{N_groups} groups each containing \code{N_i_group} individuals. 
 #' Potential outcomes exhibit spillovers: if any individual in a group receives treatment, 
 #' the effect is spread equally among members of the group. 
 #' 
-#' Note: Parameter \code{gamma} controls interactions between spillover effects. 
-#' For \code{gamma}=1 for ever $1 given to a member of a group, each member receives $1\code{group_size} no matter how many others are already treated.
+#' @details
+#' 
+#' Parameter \code{gamma} controls interactions between spillover effects.For \code{gamma}=1 for ever $1 given to a member of a group, each member receives $1\code{N_i_group} no matter how many others are already treated.
 #' For \code{gamma}>1 (<1) for ever $1 given to a member of a group, each member receives an amount that depends negatively (positively) on the number already treated.  
-#' The default estimand is the average difference 
-#' across subjects between no one treated and only that subject treated.  
-#' @param n_groups Number of groups.
-#' @param group_size Number of units in each group. May be scalar, or vector of length(n_groups) 
-#' @param sd Standard deviation of individual level shock
-#' @param gamma Parameter that controls whether spillovers within groups substitue for each other or complement each other
-#' @return A function that returns a design.
+#' 
+#' The default estimand is the average difference across subjects between no one treated and only that subject treated.  
+#' 
+#' @param N_groups An integer. Number of groups.
+#' @param N_i_group Number of units in each group. Can be scalar or vector of length \code{N_groups}.
+#' @param sd A number. Standard deviation of individual level shock.
+#' @param gamma A number. Parameter that controls whether spillovers within groups substitute or complement each other.
+#' @return A simple spillover design.
 #' @author \href{https://declaredesign.org/}{DeclareDesign Team}
 #' @concept experiment
 #' @concept spillovers
 #' @import DeclareDesign stats utils fabricatr estimatr randomizr
 #' @export
 #' @examples
-#' # To make a design using default arguments:
+#' # Generate a simple spillover design using default arguments:
 #' simple_spillover_design <- simple_spillover_designer()
 #'
 
 
-simple_spillover_designer <- function(n_groups = 80, 
-                                      group_size = 3, 
+simple_spillover_designer <- function(N_groups = 80, 
+                                      N_i_group = 3, 
                                       sd = .2,
                                       gamma = 2)
 {
   
   {{{
-    # Model ----------------------------------------------------------------------
-    population <- declare_population(G = add_level(N = n_groups, n = group_size), 
+    # M: Model
+    population <- declare_population(G = add_level(N = N_groups, n = N_i_group), 
                               i = add_level(N = n, zeros = 0, ones =1))
     
     dgp <- function(i, Z, G, n) (sum(Z[G == G[i]])/n[i])^gamma + rnorm(1)*sd
     
-    # Inquiry --------------------------------------------------------------------
+    # I: Inquiry
     estimand <- declare_estimand(Treat_1 = mean(
       sapply(1:length(G), function(i) {
         Z_i <- (1:length(G))==i
         dgp(i,Z_i,G, n) - dgp(i, zeros, G, n)})
     ), label = "estimand")
     
-    # Data
+    # D: Data Strategy
     assignment <- declare_assignment()
     
-    reveal <- declare_reveal(handler=fabricate,
+    reveal_Y <- declare_reveal(handler=fabricate,
                              Y = sapply(1:N, function(i) dgp(i, Z, G, n)))
     
-    # Answer Strategy -------------------------------------------------------------
+    # A: Answer Strategy
     estimator <- declare_estimator(Y ~ Z, estimand = estimand, 
                                    model = lm_robust, label = "naive")
     
-    # Design ----------------------------------------------------------------------
-    simple_spillover_design <- population + estimand + assignment + reveal + estimator
+    # Design
+    simple_spillover_design <- population + estimand + assignment + reveal_Y + estimator
     
   }}}
   attr(simple_spillover_design, "code") <- 
