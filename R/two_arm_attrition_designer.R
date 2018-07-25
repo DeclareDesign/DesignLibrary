6ff2c513c526b585546f3caf70e80a9a7a645b5b
Jasper Cooper <jaspercooper@gmail.com>
2018-07-25 16:59:09 -0400

    don't run slow example

--- R/two_arm_attrition_designer.R
+++ R/two_arm_attrition_designer.R
@@ -27,15 +27,19 @@
 #' @examples
 #' # To make a design using default argument (missing completely at random):
 #' two_arm_attrition_design <- two_arm_attrition_designer()
+#' \dontrun{
 #' diagnose_design(two_arm_attrition_design)
-#' 
+#' }
 #' # Attrition can produce bias even for unconditional ATE even when not
 #' # associated with treatment
+#' \dontrun{
 #' diagnose_design(two_arm_attrition_designer(b_R = 0, rho = .3))
-#'  
+#' }
 #' # Here the linear estimate using R=1 data is unbiased for
 #' # "ATE on Y (Given R)" with b_R = 0 but not when  b_R = 1   
+#' \dontrun{
 #' diagnose_design(redesign(two_arm_attrition_design, b_R = 0:1, rho = .2))
+#' }
 
 
 two_arm_attrition_designer <- function(N = 100,
