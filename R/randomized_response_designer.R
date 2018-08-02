9dda5a407b7e8438039d40dc110a02ee84c18dab
Jasper Cooper <jaspercooper@gmail.com>
2018-08-01 22:05:24 -0400

    Global variable definition

--- R/randomized_response_designer.R
+++ R/randomized_response_designer.R
@@ -25,7 +25,7 @@ randomized_response_designer <- function(N = 1000,
                                          prevalence_rate = .1,
                                          withholding_rate = .5
 ){
-  sensitive_trait <- withholder <- Y <- Z <- NULL
+  sensitive_trait <- withholder <- Y <- Z <- bias <- NULL
   if(prob_forced_yes < 0 || prob_forced_yes > 1)stop("prob_forced_yes must be in [0,1]")
   if(prevalence_rate < 0 || prevalence_rate > 1)stop("prevalence_rate must be in [0,1]")
   if(withholding_rate < 0 || withholding_rate > 1)stop("withholding_rate must be in [0,1]")
