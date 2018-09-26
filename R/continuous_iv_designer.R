46ac144c84239d663ed6dd27b946e90a5f8c35d1
Clara Bicalho <cbm343@nyu.edu>
2018-09-26 19:16:18 +0200

    Add !!

--- R/continuous_iv_designer.R
+++ R/continuous_iv_designer.R
@@ -49,10 +49,10 @@ continuous_iv_designer <- function(N = 500,
     
     catX <- categ(data[,!!treatment_name])
     
-    omegas <- sapply(catX[-1], function(x) mean((x <= data$X)*(data[,instrument_name] - mean(data[,instrument_name]))))
+    omegas <- sapply(catX[-1], function(x) mean((x <= data$X)*(data[,!!instrument_name] - mean(data[,!!instrument_name]))))
     omega  <- omegas/sum(omegas)
     
-    g_prime <- sapply(2:length(catX), function(i) mean((fy(data, catX[i]) - fy(data, catX[i-1]))))
+    g_prime <- sapply(2:length(catX), function(i) mean((fy(data, catX[i], Z = 0) - fy(data, catX[i-1], Z = 0))))
     late <- sum(g_prime*omega)
     
     z <- data[[!!instrument_name]]
