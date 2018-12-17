2b98aa48fc3de1560d7fb86283bc46351ef067dd
clabima <cbm343@nyu.edu>
2018-12-17 09:54:05 +0100

    Include design_name and fixed args

--- R/randomized_response_designer.R
+++ R/randomized_response_designer.R
@@ -26,7 +26,9 @@
 randomized_response_designer <- function(N = 1000,
                                          prob_forced_yes = .6,
                                          prevalence_rate = .1,
-                                         withholding_rate = .5
+                                         withholding_rate = .5,
+                                         design_name = "randomized_response_design",
+                                         fixed = NULL
 ){
   if(prob_forced_yes < 0 || prob_forced_yes > 1)   stop("prob_forced_yes must be in [0,1]")
   if(prevalence_rate < 0 || prevalence_rate > 1)   stop("prevalence_rate must be in [0,1]")
