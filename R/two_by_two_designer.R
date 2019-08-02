1794709fb35150003aae714f04b36336b40119ce
clabima <cbm343@nyu.edu>
2019-08-02 15:12:29 +0200

    Update warning two_by_two

--- R/two_by_two_designer.R
+++ R/two_by_two_designer.R
@@ -79,7 +79,7 @@ two_by_two_designer <- function(N = 100,
   if(max(c(prob_A, prob_B) < 0)) stop("prob_ arguments must be nonnegative")
   if(max(c(prob_A, prob_B) > 1))  stop("prob_ arguments must not exceed 1")
   if(!is.null(outcome_means) && all(!is.null(c(mean_A0B0, mean_A0B1, mean_A1B0, mean_A1B1))) &&
-     !identical(outcome_means, c(mean_A0B0, mean_A0B1, mean_A1B0, mean_A1B1)))  stop("prob_ arguments must not exceed 1")
+     !identical(outcome_means, c(mean_A0B0, mean_A0B1, mean_A1B0, mean_A1B1)))  stop("Both `outcome_means` and `mean_*` parameters are specified and have different values. Taking value of `mean_*` arguments.")
   {{{
     
     # M: Model
